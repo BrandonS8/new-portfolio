60e500e404189e0f2ca46fac63c4e500862702d3
Brandon Sears <brandonsears8@gmail.com>
2018-04-30 10:20:36 -0400

    add nav open/close

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,59 +4,111 @@ import Clipboard from 'react-clipboard.js'
 import './Nav.css'
 import me from './me.jpg'
 class Nav extends Component {
+  constructor() {
+    super()
+    this.state = {
+      open: true,
+      navStyle: {
+        display: 'flex'
+      },
+      navOpenerStyle: {
+        left: '25%',
+        tranform: 'none'
+      }
+    }
+    this.handleNavOpen = this.handleNavOpen.bind(this)
+  }
+  componentDidMount() {
+    console.log('mount')
+  }
+  handleNavOpen(e) {
+    e.preventDefault()
+    if (!this.state.open) {
+      this.setState({
+        open: true,
+        navStyle: {
+          display: 'flex'
+        },
+        navOpenerStyle: {
+          left: '25%',
+          tranform: 'none'
+        }
+      })
+    } else {
+      this.setState({
+        open: false,
+        navStyle: {
+          display: 'none'
+        },
+        navOpenerStyle: {
+          left: '0',
+          transform: 'rotate(180deg)'
+        }
+      })
+    }
+  }
   render() {
     return (
-      <nav>
-        <div class="nav-top">
-          <img src={me} alt="my face" />
-          <h1>BRANDON</h1>
-          <h1>SEARS</h1>
-          <h2>FULL STACK WEB DEVELOPER</h2>
+      <div>
+        <div
+          class="nav-opener"
+          onClick={this.handleNavOpen}
+          style={this.state.navOpenerStyle}
+        >
+          <i className="fas fa-angle-left" />
         </div>
-        <div class="nav-links">
-          <NavLink exact to="/" activeClassName="nav-link-active">
-            Home
-          </NavLink>
-          <NavLink exact to="/portfolio" activeClassName="nav-link-active">
-            Portfolio
-          </NavLink>
-          <NavLink exact to="/about" activeClassName="nav-link-active">
-            About
-          </NavLink>
-        </div>
-        <div class="nav-contact">
-          <Clipboard
-            data-clipboard-text="brandonsears8@gmail.com"
-            button-class="copy-button"
-          >
-            <i class="fas fa-clipboard" /> COPY EMAIL
-          </Clipboard>
-          <a className="contactLink" href="mailto:brandonsears8@gmail.com">
-            <p>
-              <i className="fas fa-envelope contactIcon" />{' '}
-              brandonsears8@gmail.com
-            </p>
-          </a>
-          <a
-            className="contactLink"
-            href="https://github.com/BrandonS8"
-            target="_blank"
-          >
-            <p>
-              <i className="fab fa-github contactIcon" /> /BrandonS8
-            </p>
-          </a>
-          <a
-            className="contactLink"
-            href="https://www.linkedin.com/in/brandon-sears/"
-            target="_blank"
-          >
-            <p>
-              <i className="fab fa-linkedin contactIcon" /> /brandon-sears
-            </p>
-          </a>
-        </div>
-      </nav>
+        <nav style={this.state.navStyle}>
+          <div className="nav-top">
+            <img src={me} alt="my face" />
+            <h1>BRANDON</h1>
+            <h1>SEARS</h1>
+            <h2>FULL STACK WEB DEVELOPER</h2>
+          </div>
+          <div className="nav-links">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+            <NavLink exact to="/portfolio" activeClassName="nav-link-active">
+              Portfolio
+            </NavLink>
+            <NavLink exact to="/about" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
+          <div className="nav-contact">
+            <Clipboard
+              data-clipboard-text="brandonsears8@gmail.com"
+              button-className="copy-button"
+            >
+              <i className="fas fa-clipboard" /> COPY EMAIL
+            </Clipboard>
+            <a className="contactLink" href="mailto:brandonsears8@gmail.com">
+              <p>
+                <i className="fas fa-envelope contactIcon" />{' '}
+                brandonsears8@gmail.com
+              </p>
+            </a>
+            <a
+              className="contactLink"
+              href="https://github.com/BrandonS8"
+              target="_blank"
+            >
+              <p>
+                <i className="fab fa-github contactIcon" /> /BrandonS8
+              </p>
+            </a>
+            <a
+              className="contactLink"
+              href="https://www.linkedin.com/in/brandon-sears/"
+              target="_blank"
+            >
+              <p>
+                <i className="fab fa-linkedin contactIcon" /> /brandon-sears
+              </p>
+            </a>
+          </div>
+        </nav>
+      </div>
     )
   }
 }
